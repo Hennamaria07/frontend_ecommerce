e2a80cbee042e81d7756c7d47e09029366de8efb
Henna Maria <hennamaria2001@gmail.com>
2024-03-10 12:16:53 +0530

    static best deals added

--- src/components/BestDeals.jsx
+++ src/components/BestDeals.jsx
@@ -10,84 +10,12 @@ const BestDeals = () => {
             <h3>This Month</h3>
         </div>
         <h2 className='font-bold text-4xl py-2'>Best Deals</h2>
-        <div className='grid gap-5 grid-cols-12 '>
-            <article className='col-span-3 bg-gray-50 p-5 rounded-md dark:bg-gray-800'>
-            <div className="group relative">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="" />
-        </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" className="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-            </article>
-            {/* <article className='col-span-3 bg-gray-50 p-5 rounded-md dark:bg-gray-800'>
-            <div className="group relative">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="" />
-        </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" className="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-            </article>
-            <article className='col-span-3 bg-gray-50 p-5 rounded-md dark:bg-gray-800'>
-            <div className="group relative">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="" />
-        </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" className="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-            </article>
-            <article className='col-span-3 bg-gray-50 p-5 rounded-md dark:bg-gray-800'>
-            <div className="group relative">
-        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-          <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="Front of men&#039;s Basic Tee in black." className="" />
-        </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <a href="#">
-                <span aria-hidden="true" className="absolute inset-0"></span>
-                Basic Tee
-              </a>
-            </h3>
-            <p className="mt-1 text-sm text-gray-500">Black</p>
-          </div>
-          <p className="text-sm font-medium text-gray-900">$35</p>
-        </div>
-      </div>
-            </article> */}
-            
+        <div className='grid gap-5 grid-cols-12 grid-rows-2'>
+            <div className='bg-yellow-500 col-span-5 row-span-2'>jhgfhxgvdf</div>
+            <div className='bg-red-500 col-span-2 row-span-1'>jhgfhxgvdf</div>
+            <div className='bg-green-500  col-span-2 row-span-1'>jhgfhxgvdf</div>
+            <div className='bg-purple-500  col-span-2 row-span-1'>jhgfhxgvdf</div>
+            <div className='bg-lime-500 col-span-2 row-span-1'>jhgfhxgvdf</div>
         </div>
     </section>
     </>
