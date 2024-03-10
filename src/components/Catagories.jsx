a33379e861de74d22a54b6661fae18cd3771ee32
Henna Maria <hennamaria2001@gmail.com>
2024-03-10 12:25:18 +0530

    background color updated

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -14,34 +14,34 @@ const Catagories = () => {
                 </div>
                 <h2 className='font-bold sm:text-4xl  dark:text-white text-2xl pb-5'>Browse By Category</h2>
          <div className='catagories-group grid gap-5 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2'>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
-            <div className='min-w-[170px] min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
+            <div className='min-w-[170px] bg-white dark:bg-gray-800 min-h-[145px] cursor-pointer hover:text-white hover:bg-[#043E44] text-[#043E44] dark:text-[#8CB9BD] dark:hover:text-gray-900 dark:hover:bg-[#8CB9BD] rounded-md flex justify-center items-center border-2 border-[#043E44] dark:border-[#8CB9BD]'>
                 <h5>Phones</h5>
             </div>
          </div>
